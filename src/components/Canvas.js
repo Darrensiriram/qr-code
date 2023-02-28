6b893e03b122d223e28bb6bd2dc3c894355801f5
Darren Siriram <ds.siriram@gmail.com>
2023-02-28 22:24:44 +0100

    updated after comment

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -7,10 +7,9 @@ export default function Canvas(){
   return(
   <div class='container'>
     <div class='square'> 
-      <img class='qrcode' src={Image} alt='Logo'/> 
-      <p class="wraptext">Improve your front-end skills by building projects</p>
+      <img class='qrcode' src={Image} alt='qrcode front-end'/> 
+      <h1 class="wraptext">Improve your front-end skills by building projects</h1>
       <p class="subtext"> Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
-
     </div>  
    </div> 
   ); 
