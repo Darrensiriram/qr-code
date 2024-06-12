19f6377a77e3bb9cd3f87a07629d35b36dc15160
Darren Siriram <darren.siriram@cegeka.com>
2024-06-12 14:39:03 +0200

    chore: Update favicon and page title for dummy project

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './css/Canvas.css';
-import Image from '../images/image-qr-code.png'; 
+// import Image from '../images/image-qr-code.png'; 
+import uncle_sam from '../images/uncle_sam.png';
 
 
 export default function Canvas(){
   return(
   <div class='container'>
     <div class='square'> 
-      <img class='qrcode' src={Image} alt='qrcode front-end'/> 
-      <h1 class="wraptext">Improve your front-end skills by building projects</h1>
-      <p class="subtext"> Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      <img class='qrcode' src={uncle_sam} alt='qrcode front-end'/> 
+      <h1 class="wraptext">Empower Your Innovation: Automation in Action</h1>
+      <p class="subtext"> Your Vision, Our Automation. Togethor, We Bring Ideas To Life!</p>
     </div>  
    </div> 
   ); 
